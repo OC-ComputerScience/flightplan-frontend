daf3e268b377857017d7510cfd922ae5a8f60040
ari.t <arriannaomi.a.taton@eagles.oc.edu>
2025-06-19 20:01:31 -0500

    Removed all references to Delete Task and got all of the Status references for admin (I think)

--- src/services/taskServices.js
+++ src/services/taskServices.js
@@ -35,9 +35,6 @@ export default {
   updateTask(taskId, taskData) {
     return apiClient.put(`/task/${taskId}`, taskData);
   },
-  deleteTask(taskId) {
-    return apiClient.delete(`/task/${taskId}`);
-  },
   addMajor(taskId, majorId) {
     return apiClient.post(`/task/${taskId}/majors/${majorId}`);
   },
@@ -52,6 +49,13 @@ export default {
   },
   getAllOptionalTasksForStudent(studentId, searchQuery) {
     return apiClient.get(`/task/optional/${studentId}`, {
+      params: {
+        searchQuery: searchQuery,
+      },
+    });
+  },
+  getAllActiveTasks(searchQuery) {
+    return apiClient.get("/task/active", {
       params: {
         searchQuery: searchQuery,
       },
