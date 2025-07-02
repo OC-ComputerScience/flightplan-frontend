c45422143d6bcbe93e382bfc294e08cb4dc6e409
Jenna Stilling <97480693+JennaStilling@users.noreply.github.com>
2025-07-01 19:41:16 -0500

    Feature (student pages) Consistent headers
    -Consistent title style across student pages
    -Consistent design for student flight plans when viewing as a student or admin

--- src/views/student/StudentShop.vue
+++ src/views/student/StudentShop.vue
@@ -71,6 +71,43 @@ watch(userId, () => fetchPoints(), { immediate: true });
 watch([page, searchQuery], () => fetchRewards(), { immediate: true });
 </script>
 <template>
+    <div class="pa-4">
+    <h1 class="mt-1">Student Shop</h1>
+          <v-row justify="center" class="mr-2">
+      <v-col cols="12">
+        <v-card color="backgroundDarken" style="border-radius: 25px">
+          <v-card-text>
+            <v-select
+              v-model="selectedFlightPlan"
+              :items="flightPlans"
+              :item-title="(item) => item.label"
+              :item-value="(item) => item.value"
+              variant="solo"
+              bg-color="background"
+              return-object
+              class="mb-4"
+              density="comfortable"
+              flat
+              @update:model-value="fetchFlightPlanProgress"
+            ></v-select>
+            <v-progress-linear
+              v-model="progress"
+              color="primary"
+              bg-color="backgroundLighten"
+              height="20"
+              style="border-radius: 25px"
+            >
+              <strong>{{ progress }}%</strong>
+            </v-progress-linear>
+            <div class="text-center mt-2">
+              <span class="text-subtitle-1"
+                >Available Points: {{ points }}</span
+              >
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   <v-container fluid>
     <v-row class="ml-5">
       <CardHeader
@@ -137,7 +174,9 @@ watch([page, searchQuery], () => fetchRewards(), { immediate: true });
         <p class="mb-2 text-subtitle-1">
           This reward can be redeemed by visiting the Career Services office.
         </p>
+        
       </template>
     </CardTable>
   </v-container>
+  </div>
 </template>
