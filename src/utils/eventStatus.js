52640b4607d4943175f5385a061f5e83efce3738
HeyBail <Bill331333@gmail.com>
2025-07-10 00:29:43 -0500

    fix(calendar color) past now looks for the correct event status name

--- src/utils/eventStatus.js
+++ src/utils/eventStatus.js
@@ -4,7 +4,7 @@ export const getEventCardColor = (
   registeredEvents,
   cancelledEvents,
 ) => {
-  if (event.status === "Completed") return "passed";
+  if (event.status === "Past") return "passed";
   if (cancelledEvents.some((e) => e.id === event.id)) return "canceled";
   if (checkedInEvents.some((e) => e.id === event.id)) return "checkedin";
   if (registeredEvents.some((e) => e.id === event.id)) return "registered";
