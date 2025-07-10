a632c9a5332231062dc4469c4cff537051a4b7c1
HeyBail <Bill331333@gmail.com>
2025-07-09 21:01:26 -0500

    feature(badge maintenance) added an edit button for badge task and experience rules. Will now open with popup with the selected rule.

--- src/stores/addTaskToBadgeStore.js
+++ src/stores/addTaskToBadgeStore.js
@@ -3,10 +3,25 @@ import { defineStore } from "pinia";
 export const addTaskToBadgeStore = defineStore("addTaskToBadge", {
   state: () => ({
     visible: false,
+    selectedRuleTask: null,
   }),
   actions: {
     toggleVisibility() {
       this.visible = !this.visible;
+    },
+    setSelectedRuleTask(ruleTask) {
+      this.selectedRuleTask = ruleTask;
+    },
+    clearSelectedRuleTask() {
+      this.selectedRuleTask = null;
+    },
+    addRuleTask() {
+      this.clearSelectedRuleTask();
+      this.toggleVisibility();
+    },
+    editRuleTask(ruleTask) {
+      this.setSelectedRuleTask(ruleTask);
+      this.toggleVisibility();
     },
   },
 });
--- src/views/admin/badge/BadgeAddEditPage.vue
+++ src/views/admin/badge/BadgeAddEditPage.vue
@@ -247,10 +247,16 @@ onMounted(async () => {
                 <div class="ma-2 d-flex">
                   <div class="ma-3">Quantity: {{ ruleTask.quantity }}</div>
                   <v-btn
+                    class="rounded-lg bg-warning mr-3"
+                    @click="addTaskStore.editRuleTask(ruleTask)"
+                  >
+                    <v-icon icon="mdi-pencil" color="text" size="x-large" />
+                  </v-btn>
+                  <v-btn
                     class="rounded-lg bg-danger"
-                    icon="mdi-delete"
                     @click="removeTask(ruleTask)"
                   >
+                    <v-icon icon="mdi-delete" color="text" size="x-large" />
                   </v-btn>
                 </div>
               </v-row>
@@ -258,7 +264,7 @@ onMounted(async () => {
                 <v-btn
                   block
                   class="rounded-lg bg-backgroundDarken mb-2 mt-4"
-                  @click="addTaskStore.toggleVisibility"
+                  @click="addTaskStore.addRuleTask()"
                   >Add Task</v-btn
                 ></v-row
               >
@@ -283,10 +289,18 @@ onMounted(async () => {
                     Quantity: {{ ruleExperience.quantity }}
                   </div>
                   <v-btn
+                    class="rounded-lg bg-warning mr-3"
+                    @click="
+                      addExperienceStore.editRuleExperience(ruleExperience)
+                    "
+                  >
+                    <v-icon icon="mdi-pencil" color="text" size="x-large" />
+                  </v-btn>
+                  <v-btn
                     class="rounded-lg bg-danger"
-                    icon="mdi-delete"
                     @click="removeExperience(ruleExperience)"
                   >
+                    <v-icon icon="mdi-delete" color="text" size="x-large" />
                   </v-btn>
                 </div>
               </v-row>
@@ -294,7 +308,7 @@ onMounted(async () => {
                 <v-btn
                   block
                   class="rounded-lg bg-backgroundDarken mb-2 mt-4"
-                  @click="addExperienceStore.toggleVisibility"
+                  @click="addExperienceStore.addRuleExperience()"
                   >Add Experience</v-btn
                 ></v-row
               >
