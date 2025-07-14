52df377d2a8756b6a3d311d9593e05c30e1788f5
HeyBail <Bill331333@gmail.com>
2025-07-13 19:19:32 -0500

    update(login page) changed the background picture for the login page.
    Changes
    Changed the background picture of the login page
    Removed the first column
    Made the cards transparent
    Adjusted text size and card sizing

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -53,46 +53,37 @@ const isMobile = computed(() => smAndDown.value);
   <v-container fluid class="fill-height mx-auto">
     <v-row class="fill-height login-container justify-center">
       <!-- First Column -->
-      <v-col v-if="!isMobile" class="d-flex flex-column" cols="5" xl="4">
-        <v-card
-          class="elevation-0 flex-grow-1 rounded-xl"
-          color="backgroundDarken"
-        >
-          <v-col class="fill-height d-flex flex-column">
-            <v-spacer></v-spacer>
-            <v-img class="mb-n16 ml-n16" :src="eagleSrc" />
-          </v-col>
-        </v-card>
+      <v-col v-if="!isMobile" class="d-flex flex-column" cols="6" xl="4">
       </v-col>
 
       <!-- Second Column -->
-      <v-col class="d-flex flex-column" cols="12" md="4" xl="3">
+      <v-col class="d-flex flex-column" cols="12" sm="10" md="4" xl="3">
         <v-card
           v-if="!showOnboarding"
-          class="elevation-0 flex-grow-1 rounded-xl h-25 pa-3"
-          color="backgroundDarken"
+          class="elevation-0 flex-grow-1 rounded-xl h-50 pa-3"
+          style="background-color: rgba(var(--v-theme-backgroundDarken), 0.8)"
         >
           <v-col class="fill-height d-flex flex-column">
-            <p class="text-h6">Career Services:</p>
-            <p class="text-h2 font-weight-bold">
+            <p class="text-h3">Career Services:</p>
+            <p class="text-h1 font-weight-bold">
               Eagle <br />
               Flight Plan
             </p>
             <br />
             <v-spacer></v-spacer>
-            <p class="text-h5">The World Awaits Your Story</p>
+            <p class="text-h4">The World Awaits Your Story</p>
           </v-col>
         </v-card>
         <v-card
           v-if="!showOnboarding"
           class="elevation-0 flex-grow-1 mt-5 rounded-xl"
-          color="backgroundDarken"
+          style="background-color: rgba(var(--v-theme-backgroundDarken), 0.8)"
         >
           <v-col
             class="h-75 mt-6 d-flex flex-column align-center justify-center"
           >
             <v-spacer />
-            <p class="text-center">Log in with Google to get started</p>
+            <p class="text-h5 text-center">Log in with Google to get started</p>
             <v-spacer />
             <v-card
               class="elevation-0 h-auto py-7 px-6 rounded-lg"
@@ -114,6 +105,15 @@ const isMobile = computed(() => smAndDown.value);
 <style scoped>
 .login-container {
   max-height: 800px;
+  background-image: url("../assets/eagle-login-background.jpg");
+  background-size: cover;
+  background-position: center;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  margin-left: -10px;
+  box-shadow: 20 8px 12px rgba(0, 0, 0, 20);
+  overflow: hidden;
 }
 
 /* Enter and leave animations */
